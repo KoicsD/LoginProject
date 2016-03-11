d06f16a8cc3a2e641a17a6b4a94b432b47339fed
Daniel Koics <koics.dani@gmail.com>
2016-03-11 13:36:26 +0100

    overriding doGet and doPost in servlet class begun

--- src/dancipage/DanciServlet.java
+++ src/dancipage/DanciServlet.java
@@ -1,24 +1,42 @@
 package dancipage;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class DanciServlet extends HttpServlet {
+	
+	private static final Map<String, String> passwords = new HashMap<>();
+	static {
+		passwords.put("Danci", "icnaD");
+		passwords.put("KoicsD", "blabla");
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		HttpSession session = req.getSession();
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("Thank you for requesting your profile!"); //TODO: get-profile logic
+		out.close();
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		HttpSession session = req.getSession();
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("Thank you for posting me your data!"); //TODO: post-password logic
+		out.close();
 	}
 
 }
