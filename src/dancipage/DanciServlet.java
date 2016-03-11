0b25f4f9b9d6c3000a46b50ea804a1ded8aff175
Daniel Koics <koics.dani@gmail.com>
2016-03-11 14:46:56 +0100

    get logic written, except html body of redirectUserToLogin

--- src/dancipage/DanciServlet.java
+++ src/dancipage/DanciServlet.java
@@ -25,7 +25,11 @@ public class DanciServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		resp.setCharacterEncoding("UTF-8");
 		PrintWriter out = resp.getWriter();
-		out.println("Thank you for requesting your profile!"); //TODO: get-profile logic
+		Object username = session.getAttribute("username");
+		if (username != null && passwords.containsKey(username))
+			greetUser(out, (String)username);
+		else
+			redirectUserToLogin(out);
 		out.close();
 	}
 
@@ -36,7 +40,40 @@ public class DanciServlet extends HttpServlet {
 		resp.setCharacterEncoding("UTF-8");
 		PrintWriter out = resp.getWriter();
 		out.println("Thank you for posting me your data!"); //TODO: post-password logic
+		//session.invalidate();
+		//session.setAttribute("username", "Danci");
 		out.close();
 	}
 
+	private void greetUser(PrintWriter out, String username) {
+		String title = "Profile";
+		String body = "<h1>Welcome, " + username + ". Have a nice day.</h1>";
+		out.print(generateHTML(title, body));
+	}
+	
+	private void redirectUserToLogin(PrintWriter out) {
+		String title = "UnauthorizedAccess";
+		String body = "";
+		out.print(generateHTML(title, body));
+	}
+	
+	private void warnUserAlreadyLoggedIn(PrintWriter out) {
+		String title = "AlreadyLoggedIn";
+		String body = "";
+		out.print(generateHTML(title, body));
+	}
+	
+	private String generateHTML(String title, String body) {
+		String html = "";
+		html += "<html>\n";
+		html += "<head>\n";
+		html += "<title>" + title + "</title>\n";
+		html += "</head>\n";
+		html += "<body>\n";
+		html += body;
+		html += "</body>\n";
+		html += "</html>\n";
+		return html;
+	}
+	
 }
